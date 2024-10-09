2a3946e09ea982450bb78d524fa096f759dd4ba8
LeeTheY <peter20011148@gmail.com>
2024-10-09 23:15:44 +0900

    “upload”

---
+++ App.js
@@ -0,0 +1,130 @@
+import React, { useState, useEffect } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import SplashScreen from './src/Screens/SplashScreen';
+
+import LoginScreen from './src/Screens/LoginScreen';
+
+import SignUpScreen from './src/Screens/SignUpScreen';
+import AuthenticationScreen from './src/Screens/AuthenticationScreen';
+
+import GuestScreen from './src/Screens/GuestScreen';
+
+
+
+import RouteScreen from './src/Navigation/RouteScreen';
+
+import HomeScreen from './src/Screens/HomeScreen';
+
+
+import CommuScreen from './src/Screens/CommuScreen';
+import FreeBoardScreen from './src/Screens/FreeBoardScreen';
+
+import ChatScreen from './src/Screens/ChatScreen';
+import ChatDetailPage from './src/Screens/Pages/ChatDetailPage';
+
+import HomeScrapScreen from './src/Screens/HomeScrapScreen';
+import CommuScrapScreen from './src/Screens/CommuScrapScreen';
+import FreeBoardScrapScreen from './src/Screens/FreeBoardScrapScreen';
+
+import ProfileScreen from './src/Screens/ProfileScreen';
+
+
+import HomePostPage from './src/Screens/Pages/HomePostPage';
+import CommuPostPage from './src/Screens/Pages/CommuPostPage';
+import FreeBoardPostPage from './src/Screens/Pages/FreeBoardPostPage';
+
+import NotificationPage from './src/Screens/Pages/NotificationPage';
+import SearchingPage from './src/Screens/Pages/SearchingPage';
+import SettingPage from './src/Screens/Pages/SettingPage';
+
+import SettingModal from './src/Screens/Pages/SettingModal';
+
+
+
+import HomeDetailPage from './src/Screens/Pages/HomeDetailPage';
+import CommuDetailPage from './src/Screens/Pages/CommuDetailPage';
+import FreeBoardDetailPage from './src/Screens/Pages/FreeBoardDetailPage';
+
+
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+
+  const [isShowSplash, setIsShowSplash] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsShowSplash(false);
+    }, 1500);
+  })
+
+  return (
+    <>
+      {isShowSplash ? (<SplashScreen />) : (
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="LoginScreen">
+
+
+            <Stack.Screen
+              name="LoginScreen"
+              component={LoginScreen}
+              options={{ headerShown: false }}
+            />
+
+            <Stack.Screen
+              name="SignUpScreen"
+              component={SignUpScreen}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="AuthenticationScreen"
+              component={AuthenticationScreen}
+              options={{ headerShown: false }}
+            />
+
+            <Stack.Screen
+              name="GuestScreen"
+              component={GuestScreen}
+              options={{ headerShown: false }}
+            />
+
+
+            <Stack.Screen name="RouteScreen" component={RouteScreen} options={{ headerShown: false }} />
+            <Stack.Screen name="HomeScreen" component={HomeScreen} options={{ headerShown: false }} />
+
+            <Stack.Screen name="CommuScreen" component={CommuScreen} options={{ headerShown: false }} />
+            <Stack.Screen name="FreeBoardScreen" component={FreeBoardScreen} options={{ headerShown: false }} />
+
+            <Stack.Screen name="ChatScreen" component={ChatScreen} options={{ headerShown: false }} />
+            <Stack.Screen name="ChatDetailPage" component={ChatDetailPage} options={{ headerShown: false }} />
+
+            <Stack.Screen name="HomeScrapScreen" component={HomeScrapScreen} options={{ headerShown: false }} />
+            <Stack.Screen name="CommuScrapScreen" component={CommuScrapScreen} options={{ headerShown: false }} />
+            <Stack.Screen name="FreeBoardScrapScreen" component={FreeBoardScrapScreen} options={{ headerShown: false }} />
+
+            <Stack.Screen name="ProfileScreen" component={ProfileScreen} options={{ headerShown: false }} />
+
+
+            <Stack.Screen name="HomePostPage" component={HomePostPage} options={{ headerShown: false }} />
+            <Stack.Screen name="CommuPostPage" component={CommuPostPage} options={{ headerShown: false }} />
+            <Stack.Screen name="FreeBoardPostPage" component={FreeBoardPostPage} options={{ headerShown: false }} />
+
+            <Stack.Screen name="NotificationPage" component={NotificationPage} options={{ headerShown: false }} />
+            <Stack.Screen name="SearchingPage" component={SearchingPage} options={{ headerShown: false }} />
+            <Stack.Screen name="SettingPage" component={SettingPage} options={{ headerShown: false }} />
+            <Stack.Screen name="SettingModal" component={SettingModal} options={{ headerShown: false }} />
+
+
+            <Stack.Screen name="HomeDetailPage" component={HomeDetailPage} options={{ headerShown: false }} />
+            <Stack.Screen name="CommuDetailPage" component={CommuDetailPage} options={{ headerShown: false }} />
+            <Stack.Screen name="FreeBoardDetailPage" component={FreeBoardDetailPage} options={{ headerShown: false }} />
+
+          </Stack.Navigator>
+        </NavigationContainer>
+      )}
+    </>
+  );
+};
---
+++ src/Screens/Pages/SettingPage.js
@@ -0,0 +1,228 @@
+import { StatusBar } from "expo-status-bar";
+import {
+    StyleSheet,
+    Text,
+    TouchableOpacity,
+    View,
+    Image,
+    SafeAreaView,
+    ScrollView,
+} from "react-native";
+import { useNavigation } from "@react-navigation/native"; // useNavigation 훅 추가
+
+import { Switch } from "react-native-gesture-handler";
+import { useState } from "react";
+import SettingModal from './SettingModal.js';  // 모달 import 추가
+
+import IMAGES from "../../../assets";
+
+
+export default function SettingPage() {
+    const navigation = useNavigation(); // useNavigation 훅으로 navigation 객체 가져오기
+
+    //토글 스위치 상태 관리
+    const [isChatEnabled, setIsChatEnabled] = useState(false);
+    const [isCommunityEnabled, setIsCommunityEnabled] = useState(false);
+
+
+    const [nickname, setNickname] = useState('어서옵쇼'); // Default nickname
+    // 모달 가시성 상태 관리
+    const [isModalVisible, setIsModalVisible] = useState(false);
+    const [profileImage, setProfileImage] = useState(IMAGES.LOGO); // 기본 프로필 이미지
+
+    return (
+        <ScrollView contentContainerStyle={styles.scrollContainer}>
+            <SafeAreaView style={styles.container}>
+                <View style={styles.topContainer}>
+                    <View style={styles.topBtnContainer}>
+                        <TouchableOpacity
+                            onPress={() => navigation.goBack()} // 뒤로가기 네비게이션
+                        >
+                            <Image
+                                source={IMAGES.LOGOV2}
+                                style={styles.backImg}
+                            />
+                        </TouchableOpacity>
+                    </View>
+                    <View style={styles.topTitleContainer}>
+                        <Text style={styles.topFont}>설정</Text>
+                    </View>
+                    <View style={{ width: "15%" }}></View>
+                </View>
+
+                <View style={styles.myContainer}>
+                    <View style={styles.myImg}>
+                        <TouchableOpacity onPress={() => setIsModalVisible(true)}>
+                            <Image
+                                source={profileImage}
+                                style={styles.backImg}
+                            />
+                        </TouchableOpacity>
+                    </View>
+
+                    <View style={styles.myName}>
+                        <Text style={[{ fontSize: 16 }]}>{nickname} 님</Text>
+                    </View>
+                </View>
+                <View style={styles.mainContainer}>
+                    <View style={styles.menuBox}>
+                        <View style={styles.noneLineBox}>
+                            <Text style={[styles.menuFontBold, { marginTop: 10 }]}>계정</Text>
+                        </View>
+
+                        <TouchableOpacity style={styles.noneLineBox} onPress={() => navigation.navigate("MyInfo")}>
+                            <Text style={styles.menuFont}>내 정보</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styles.noneLineBox}>
+                            <Text style={styles.menuFont}>비밀번호 변경</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styles.noneLineBox}>
+                            <Text style={styles.menuFont}>주소 변경</Text>
+                        </TouchableOpacity>
+
+                        <View style={styles.lineBox}>
+                            <Text style={styles.menuFontBold}>알림</Text>
+                        </View>
+
+
+                        <View style={styles.switchLineBox}>
+                            <Text style={styles.menuFont}>채팅 알림</Text>
+                            <Switch
+                                value={isChatEnabled}
+                                onValueChange={setIsChatEnabled}
+                            />
+                        </View>
+
+                        <View style={styles.switchLineBox}>
+                            <Text style={styles.menuFont}>커뮤니티 알림</Text>
+                            <Switch
+                                value={isCommunityEnabled}
+                                onValueChange={setIsCommunityEnabled}
+                            />
+                        </View>
+
+                        <View style={styles.lineBox}>
+                            <Text style={styles.menuFontBold}>기타</Text>
+                        </View>
+
+                        <TouchableOpacity style={styles.noneLineBox}>
+                            <Text style={styles.menuFont}>언어 설정</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styles.noneLineBox} onPress={() => navigation.navigate("Login")}>
+                            <Text style={styles.menuFont}>로그아웃</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styles.noneLineBox}>
+                            <Text style={[styles.menuFont, { marginBottom: 10 }]}>탈퇴하기</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+
+                <StatusBar style="auto" />
+                <SettingModal
+                    isVisible={isModalVisible}
+                    onClose={() => setIsModalVisible(false)}  // 모달 닫는 함수
+                    setNickname={setNickname}
+                    setProfileImage={setProfileImage}  // 이 부분을 추가하여 프로필 이미지 변경 함수 전달
+                />
+
+            </SafeAreaView>
+        </ScrollView>
+    );
+}
+
+const styles = StyleSheet.create({
+    scrollContainer: {
+        flexGrow: 1, // ScrollView가 화면 전체를 차지하게 설정
+    },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    topContainer: {
+        width: "100%",
+        height: "10%",
+        paddingTop: 20,
+        flexDirection: "row", // 이미지와 텍스트를 가로로 배치
+    },
+    topBtnContainer: {
+        width: "15%",
+    },
+    topTitleContainer: {
+        width: "70%",
+    },
+    myContainer: {
+        width: "100%",
+        height: "8%",
+        flexDirection: "row",
+    },
+    myImg: {
+        width: "25%",
+        justifyContent: "center",
+        alignItems: "flex-end",
+        paddingRight: "5%",
+    },
+    myName: {
+        width: "70%",
+        justifyContent: "center",
+        fontwei: "bold",
+        fontSize: 16,
+    },
+    mainContainer: {
+        width: "100%",
+        height: "75%",
+        alignItems: "center",
+    },
+    backImg: {
+        height: 44,
+        width: 44,
+        justifyContent: "flex-start",
+        borderRadius: 22,
+    },
+    topFont: {
+        fontSize: 20,
+        marginTop: 5,
+        textAlign: "center",
+        justifyContent: "center",
+    },
+    menuBox: {
+        width: "90%",
+        justifyContent: "center",
+        borderRadius: 20,
+        borderWidth: 1,
+        borderColor: "#ccc",
+        marginTop: 12,
+        backgroundColor: "#fff",
+    },
+    menuFont: {
+        fontSize: 15,
+    },
+    menuFontBold: {
+        fontSize: 16,
+        fontWeight: "bold",
+    },
+    lineBox: {
+        flexDirection: "row", // 이미지와 텍스트를 가로로 배치
+        alignItems: "center", // 이미지와 텍스트를 수직으로 가운데 정렬
+        padding: 10, // 패딩 추가로 터치 영역 확대
+        paddingTop: 16,
+        marginHorizontal: 13,
+        marginTop: 10,
+        borderTopWidth: 1, // 각 메뉴 항목 아래에 구분선 추가
+        borderTopColor: "#E5E5E5",
+    },
+    noneLineBox: {
+        flexDirection: "row", // 이미지와 텍스트를 가로로 배치
+        marginHorizontal: 13,
+        alignItems: "center", // 이미지와 텍스트를 수직으로 가운데 정렬
+        padding: 10, // 패딩 추가로 터치 영역 확대
+    },
+    switchLineBox: {
+        flexDirection: "row",
+        justifyContent: "space-between", // 텍스트와 스위치가 양 끝에 배치되도록 설정
+        marginHorizontal: 10,
+        alignItems: "center", // 스위치와 텍스트를 수직 가운데 정렬
+        paddingHorizontal: 12
+    },
+});
